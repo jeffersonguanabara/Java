aec80f456715b858e55cbc9b0d2ecbfac976b6ed
guanabara <guanabara@guanabara-Inspiron-N4050>
2019-06-04 21:42:18 -0300

    "voltando aos fundamentos de java"

---
+++ Caelum_FJ11_Orientacao_a_Objetos_Com_Java/br/caelum/FJ11/Capitulo_2/MeuPrograma.java
@@ -0,0 +1,22 @@
+package br.caelum.FJ11.Capitulo_2;
+
+public class MeuPrograma {
+
+	public static void main(String[] args) {
+		
+		// primeiro exemplo
+		//System.out.println("Minha primeira aplicação Java!");
+		
+		// mudando a frase do exemplo
+		//System.out.println("Exibindo novo texto!");
+		
+		// quebrando uma frase usando 2 System.out...
+		//System.out.println("Exibindo novo");
+		//System.out.println("texto!");
+		
+		// quebrando uma frase usando a quebra de linha do java \n
+		System.out.println("Exibindo novo \ntexto");
+		
+		
+	}
+}
